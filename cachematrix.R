a37cd00efb1cc6dee39643a10b683082bc6c8e5f
prashanthmkt <prashanthkumar.mkt@gmail.com>
2014-11-20 21:05:17 +0530

    updated Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+m <- NULL
+set <- function(y){
+x <<- y
+m <<- NULL
+}
+get <- function() x
+setmatrix <- function(mean) m <<- mean
+getmatrix <- function() m
+list(set =set, get=get,
+setmatrix=setmatrix,
+getmatrix = getmatrix)
 
+}
+cacheSolve <- function(x,....) {
+m <- x$getmatrix()
+if(!is.null(m)) {
+message("getting cached data")
+return(m)
+}
+data <- X$get()
+m <-matrix(data, ...)
+x$setmatrix(m)
+m
 }
 
 
